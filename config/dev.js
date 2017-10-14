db5aa60b9f8b6c00be73a82223761f62a682da0b
Abid Rakhmansyah <bidnth@gmail.com>
2017-10-15 01:35:07 +0700

    Update Redux

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: '',
-  googleClientSecret: '',
-  mongoURI: '',
-  cookieKey: ''
+  googleClientID: '688449945295-cjco0c0dl00ud80mmjl1ulk9i2qcsesu.apps.googleusercontent.com',
+  googleClientSecret: 'gz49BkC6YWtWgn7VPrccXiDl',
+  mongoURI: 'mongodb://abid:password@ds013475.mlab.com:13475/abid-dev',
+  cookieKey: 'wef234k24m2lk34lklksdf331221'
 }
